5aa378afd30b2c5708e5b4d2cfb3000b109822e5
Shahzaib <shahzaib@codewithshaz.com>
2023-08-18 15:31:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomnumber1=Math.floor(Math.random()* 6) + 1;  
+var Randomdiceimage="dice" + randomnumber1 + ".png";
+var randomImageSource="image/" + Randomdiceimage;
+var image1=document.querySelectorAll ("img")[0];
+image1.setAttribute("src", randomImageSource); 
+ 
+var randomnumber2=Math.floor(Math.random()* 6) + 1;
+var RandomImageSource2="image/dice" + randomnumber2 + ".png"; 
+document.querySelectorAll("img")[1].setAttribute("src", RandomImageSource2);
+
+if(randomnumber1>randomnumber2){
+    document.querySelector("h1").innerHTML="Player 1 Wins!";
+} 
+else if(randomnumber2>randomnumber1){
+    document.querySelector("h1").innerHTML="Player 2 Wins!";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!";
+}
